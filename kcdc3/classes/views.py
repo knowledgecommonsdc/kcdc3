e10c1a61cf827de7192b8236bd388f93aee1409f
David Ramos <dave@imaginaryterrain.com>
2012-08-01 00:54:20 -0400

    get registration and cancellation and waitlists up

--- kcdc3/classes/views.py
+++ kcdc3/classes/views.py
@@ -1,21 +1,79 @@
-from django.shortcuts import render_to_response
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from datetime import datetime
+from django.views.generic import DetailView
 from classes.models import Event, Registration
+	
+	
+	
+class EventDetailView(DetailView):
 
-def index(request):
-	class_list = Event.objects.all().order_by('date')
-	return render_to_response('classes/index.html', {'class_list': class_list})
+	context_object_name = "event"
+	model = Event
 	
-def single(request, slug):
-	event = Event.objects.get(slug=slug)
-	registration_count = Event.objects.filter(slug=slug, registration__waitlist=False, registration__cancelled=False).count()
-	waitlist_count = Event.objects.filter(slug=slug, registration__waitlist=True, registration__cancelled=False).count()
-	return render_to_response('classes/single.html', {'event': event, 'registration_count': registration_count, 'waitlist_count': waitlist_count})
-	
+	def get_context_data(self, **kwargs):
+		
+		context = super(EventDetailView, self).get_context_data(**kwargs)
+		context['registration_count'] = Registration.objects.filter(event=self.object, waitlist=False, cancelled=False).count()
+		context['waitlist_count'] = Registration.objects.filter(event=self.object, waitlist=True, cancelled=False).count()
+
+		if (Registration.objects.filter(event=self.object, student=self.request.user, waitlist=True, cancelled=False).count() > 0):
+			context['user_is_waitlisted'] = True
+		else:
+			context['user_is_waitlisted'] = False
+
+		if (Registration.objects.filter(event=self.object, student=self.request.user, waitlist=False, cancelled=False).count() > 0):
+			context['user_is_registered'] = True
+		else:
+			context['user_is_registered'] = False
+
+		return context
+			
+
+
 def register(request, slug):
-	event = Event.objects.get(slug=slug)
-	r = Registration(student=request.user, event=event, date_registered=datetime.now(), waitlist=False)
-	r.save()
-	return render_to_response('classes/register-response.html', {'event': event})
 	
+	e = Event.objects.get(slug=slug)
+	registration_count = Registration.objects.filter(event=e, waitlist=False, cancelled=False).count()
+	curr_user_registration_count = Registration.objects.filter(event=e, student=request.user, cancelled=False).count()
+
+	if registration_count >= e.max_students:
+		waitlist = True
+	else:
+		waitlist = False
+
+	# if there are no non-cancelled registrations for this user and event
+	if curr_user_registration_count == 0:
+		r = Registration(student=request.user, event=e, date_registered=datetime.now(), waitlist=waitlist)
+		r.save()
+		if waitlist == False:
+			return HttpResponseRedirect("/classes/registered")
+		else:
+			return HttpResponseRedirect("/classes/waitlisted")
+	else: 
+		return HttpResponseRedirect("/classes/error")
+
+
+
+def cancel(request, slug):
+	
+	e = Event.objects.get(slug=slug)
+	registration_count = Registration.objects.filter(event=e, waitlist=False, cancelled=False).count()
+	curr_user_registration_count = Registration.objects.filter(event=e, student=request.user, cancelled=False).count()
+
+	if registration_count >= e.max_students:
+		waitlist = True
+	else:
+		waitlist = False
+
+	if curr_user_registration_count > 0:
+		for r in Registration.objects.filter(event=e, student=request.user, cancelled=False)[:1]:
+			r.date_cancelled=datetime.now()
+			r.cancelled=True
+			r.save()
+		if waitlist==True and r.waitlist==False:
+		 	for n in Registration.objects.filter(event=e, waitlist=True, cancelled=False)[:1]:
+				n.waitlist=False
+				n.save()
+		return HttpResponseRedirect("/classes/cancelled")
+	else: 
+		return HttpResponseRedirect("/classes/error")
