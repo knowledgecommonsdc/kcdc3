c274d3823803b943c9c07c622c1a649952064960
David Ramos <dave@imaginaryterrain.com>
2012-08-01 16:55:09 -0400

    clean up class views - writer helper class

--- kcdc3/classes/views.py
+++ kcdc3/classes/views.py
@@ -2,9 +2,10 @@ from django.http import HttpRequest, HttpResponseRedirect
 from datetime import datetime
 from django.views.generic import DetailView
 from classes.models import Event, Registration
+
+
 	
-	
-	
+# display an event	
 class EventDetailView(DetailView):
 
 	context_object_name = "event"
@@ -30,22 +31,17 @@ class EventDetailView(DetailView):
 			
 
 
+# handle registration/waitlist form
 def register(request, slug):
 	
 	e = Event.objects.get(slug=slug)
-	registration_count = Registration.objects.filter(event=e, waitlist=False, cancelled=False).count()
-	curr_user_registration_count = Registration.objects.filter(event=e, student=request.user, cancelled=False).count()
-
-	if registration_count >= e.max_students:
-		waitlist = True
-	else:
-		waitlist = False
+	r = RegistrationHelper(e,request.user)
 
 	# if there are no non-cancelled registrations for this user and event
-	if curr_user_registration_count == 0:
-		r = Registration(student=request.user, event=e, date_registered=datetime.now(), waitlist=waitlist)
-		r.save()
-		if waitlist == False:
+	if r.user_is_registered==False and r.user_is_waitlisted==False:
+		t = Registration(student=request.user, event=e, date_registered=datetime.now(), waitlist=r.add_to_waitlist)
+		t.save()
+		if r.add_to_waitlist == False:
 			return HttpResponseRedirect("/classes/registered")
 		else:
 			return HttpResponseRedirect("/classes/waitlisted")
@@ -54,26 +50,49 @@ def register(request, slug):
 
 
 
+# handle cancel form
 def cancel(request, slug):
 	
 	e = Event.objects.get(slug=slug)
-	registration_count = Registration.objects.filter(event=e, waitlist=False, cancelled=False).count()
-	curr_user_registration_count = Registration.objects.filter(event=e, student=request.user, cancelled=False).count()
-
-	if registration_count >= e.max_students:
-		waitlist = True
-	else:
-		waitlist = False
-
-	if curr_user_registration_count > 0:
-		for r in Registration.objects.filter(event=e, student=request.user, cancelled=False)[:1]:
-			r.date_cancelled=datetime.now()
-			r.cancelled=True
-			r.save()
-		if waitlist==True and r.waitlist==False:
-		 	for n in Registration.objects.filter(event=e, waitlist=True, cancelled=False)[:1]:
-				n.waitlist=False
-				n.save()
+	r = RegistrationHelper(e,request.user)
+	
+	if r.user_is_registered or r.user_is_waitlisted:
+		for t in Registration.objects.filter(event=e, student=request.user, cancelled=False)[:1]:
+			t.date_cancelled=datetime.now()
+			t.cancelled=True
+			t.save()
+		if r.add_to_waitlist==True and e.waitlist_status==True:
+		 	for w in Registration.objects.filter(event=e, waitlist=True, cancelled=False)[:1]:
+				w.waitlist=False
+				w.save()
 		return HttpResponseRedirect("/classes/cancelled")
 	else: 
 		return HttpResponseRedirect("/classes/error")
+
+
+# provide information about an event's registration status
+# relative to a particular event and user
+class RegistrationHelper:
+		
+	def __init__(self, event, student):
+
+		self.e = event
+		self.s = student
+
+		registration_count = Registration.objects.filter(event=self.e, waitlist=False, cancelled=False).count()
+		waitlist_count = Registration.objects.filter(event=self.e, waitlist=True, cancelled=False).count()
+
+		if registration_count >= self.e.max_students:
+			self.add_to_waitlist = True
+		else:
+			self.add_to_waitlist = False
+
+		self.user_is_waitlisted = False
+		self.user_is_registered = False
+		if (Registration.objects.filter(event=self.e, student=self.s, waitlist=False, cancelled=False).count() > 0):
+			self.user_is_registered = True
+		elif (Registration.objects.filter(event=self.e, student=self.s, waitlist=True, cancelled=False).count() > 0):
+			self.user_is_waitlisted = True
+	
+	
+	
