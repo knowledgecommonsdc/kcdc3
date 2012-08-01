3b0f64f40104798d1b9d35431a653e79a333d919
David Ramos <dave@imaginaryterrain.com>
2012-08-01 17:33:10 -0400

    add message handling

--- kcdc3/classes/urls.py
+++ kcdc3/classes/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from classes.models import Event, Registration
 from django.views.generic import ListView
-from classes.views import EventDetailView
+from classes.views import EventDetailView, ResponseTemplateView
 
 urlpatterns = patterns('classes.views',
 
 	url(r'^$', ListView.as_view(model=Event,)),
+	url(r'^response/(?P<slug>[A-Za-z0-9_-]+)$', ResponseTemplateView.as_view()),
 	url(r'^(?P<slug>[A-Za-z0-9_-]+)/$', EventDetailView.as_view(model=Event,)),
 	url(r'^(?P<slug>[A-Za-z0-9_-]+)/register$', 'register'),
 	url(r'^(?P<slug>[A-Za-z0-9_-]+)/cancel$', 'cancel'),
