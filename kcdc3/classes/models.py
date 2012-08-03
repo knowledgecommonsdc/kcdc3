29076027502c737aff04dc1b29c14a414d54feb7
David Ramos <dave@imaginaryterrain.com>
2012-08-03 12:15:04 -0400

    add fields and update admin

--- kcdc3/classes/models.py
+++ kcdc3/classes/models.py
@@ -4,14 +4,48 @@ from django.contrib.auth.models import User
 class Event(models.Model):
 	
 	title = models.CharField(max_length=200)
-	date = models.DateTimeField('First meeting')
 	slug = models.SlugField(unique=True)
+	STATUS_CHOICES = (
+		('PUBLISHED', 'Published'),
+		('HIDDEN', 'Hidden'),
+		('DRAFT', 'Draft'),
+		('REMOVED', 'Removed'),
+	)
+	status = models.CharField(max_length=9, choices=STATUS_CHOICES, default='PUBLISHED')
+	featured = models.BooleanField(default=False)
+	
+	date = models.DateTimeField('First meeting')
+	additional_dates_text = models.TextField('Notes about additional meetings', blank=True)
+
+	location_name = models.CharField(max_length=100, blank=True)
+	location_address1 = models.CharField('address', max_length=60, blank=True)
+	location_address2 = models.CharField('line two', max_length=60, blank=True)
+	location_city = models.CharField('city', max_length=60, blank=True, default='Washington')
+	location_state = models.CharField('state', max_length=2, blank=True, default='DC')
+	location_zip = models.CharField('ZIP', max_length=5, blank=True)
+	location_description = models.TextField('Location details', blank=True)
+	location_show_exact = models.BooleanField('Show details/exact address on public site?', default=True)
+	
+	TYPE_CHOICES = (
+		('CLASS', 'Class'),
+		('EVENT', 'Event'),
+		('EXTERNAL', 'Non-KCDC Event'),
+	)
+	type = models.CharField(max_length=9, choices=TYPE_CHOICES, default='CLASS')
+
 	summary = models.TextField(blank=True)
 	description = models.TextField(blank=True)
-	location_description = models.TextField(max_length=200, blank=True)
+	thumbnail = models.ImageField(upload_to='event_images', blank=True, null=True)
+	main_image = models.ImageField(upload_to='event_images', blank=True, null=True)
+
+	email_welcome_text = models.TextField('Additional welcome email text', blank=True)
+	email_reminder = models.BooleanField('Send reminder email?', default=True)
+	email_reminder_text = models.TextField('Additional reminder email text', blank=True)
+
+	documentation = models.TextField(blank=True)
 	
-	max_students = models.IntegerField('Max size', blank=True, null=True)
-	waitlist_status = models.BooleanField('Waitlist', default=True)
+	max_students = models.IntegerField('Max students', blank=True, null=True)
+	waitlist_status = models.BooleanField('Use waitlist', default=True)
 	
 	REGISTRATION_STATUS_CHOICES = (
 		('AUTO', 'Follow session settings'),
@@ -22,6 +56,7 @@ class Event(models.Model):
 	registration_status = models.CharField(max_length=7, choices=REGISTRATION_STATUS_CHOICES, default='AUTO')
 
 	teachers = models.ManyToManyField(User, blank=True, null=True, related_name='teachers')
+	facilitators = models.ManyToManyField(User, blank=True, null=True, related_name='facilitators')
 	students = models.ManyToManyField(User, through='Registration', blank=True, null=True, related_name='students')
 		
 	class Meta:
