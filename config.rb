5fac0944b7ef7c83f6f61844eaee41e1d7d83b8c
David Ramos <dave@imaginaryterrain.com>
2012-08-19 20:51:16 -0400

    Set up SASS and ignore CodeKit

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "kcdc3/public/static/css"
+sass_dir = "kcdc3/sass"
+images_dir = "kcdc3/public/static/images"
+javascripts_dir = "kcdc3/public/static/js"
+output_style = :compressed
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
