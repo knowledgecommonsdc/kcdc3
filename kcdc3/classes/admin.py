2cf4ea75b9087ee3a6447bdfea57dd6ca81c21a3
David Ramos <dave@imaginaryterrain.com>
2012-08-21 11:58:11 -0400

    facilitator: add default address to lists; make more registration fields readonly in admin

--- kcdc3/classes/admin.py
+++ kcdc3/classes/admin.py
@@ -17,7 +17,7 @@ class RegistrationInline(admin.TabularInline):
 	# inline_classes = ('grp-collapse grp-open',)
 	fields = ('student', 'date_registered', 'waitlist', 'cancelled', 'date_cancelled', 'attended')
 	can_delete = False
-	readonly_fields = ('date_registered','date_cancelled')
+	readonly_fields = ('date_registered','date_cancelled','waitlist')
 	raw_id_fields = ['student']
 	related_lookup_fields = {
 	    'fk': ['student'],
