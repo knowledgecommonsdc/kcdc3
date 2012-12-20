fe280485f44f4160e8a0f62558e0c7dd533a7e6d
David Ramos <dave@imaginaryterrain.com>
2012-12-20 09:15:22 -0500

    Fix for archive display
    Apparently I did not merge branches correctly. This fix lets the site
    show event details.

--- kcdc3/classes/urls.py
+++ kcdc3/classes/urls.py
@@ -5,7 +5,7 @@ from classes.views import EventListView, EventDetailView, ResponseTemplateView, 
 urlpatterns = patterns('classes.views',
 
 	url(r'^$', EventListView.as_view()),
-	url(r'^(?P<slug>[A-Za-z0-9_-]+)/$', EventArchiveView.as_view()),
+	url(r'^(?P<slug>[0-9]+)/$', EventArchiveView.as_view()),
 	url(r'^response/(?P<slug>[A-Za-z0-9_-]+)$', ResponseTemplateView.as_view()),
 	url(r'^(?P<slug>[A-Za-z0-9_-]+)/$', EventDetailView.as_view(model=Event,)),
 	url(r'^(?P<slug>[A-Za-z0-9_-]+)/register$', 'register'),
