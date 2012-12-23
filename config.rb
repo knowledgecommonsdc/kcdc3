6d1480171355ad3d6da87d60d513068b208acd4b
David Ramos <dave@imaginaryterrain.com>
2012-12-23 12:59:59 -0500

    Bring in new CSS, scripts, and images from redesign branch; remove unneeded files for old layouts

--- config.rb
+++ config.rb
@@ -1,12 +1,10 @@
-# This is for converting SASS files into CSS using SASS/Compass
-
+# Config file for SASS/Compass
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "kcdc3/public/static/css"
-sass_dir = "kcdc3/sass"
+sass_dir = "kcdc3/public/static/sass"
 images_dir = "kcdc3/public/static/images"
-javascripts_dir = "kcdc3/public/static/js"
-output_style = :compressed
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+javascripts_dir = "kcdc3/public/static/assets/js"
+output_style = :expanded
+css_dir = "kcdc3/public/static/css"
+
