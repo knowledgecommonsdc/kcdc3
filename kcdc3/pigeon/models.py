fca1ff415a8d76e294f5e4fc52c92248a615dff0
David Ramos <dave@imaginaryterrain.com>
2013-02-16 01:34:35 -0500

    Publish blog posts through an RSS feed

--- kcdc3/pigeon/models.py
+++ kcdc3/pigeon/models.py
@@ -33,4 +33,6 @@ class Post(models.Model):
 	def __unicode__(self):
 		return self.title
 
-
+	@models.permalink
+	def get_absolute_url(self):
+		return ('post_view', [str(self.slug)])
--- kcdc3/pigeon/urls.py
+++ kcdc3/pigeon/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from pigeon.models import Post
-from pigeon.views import PostListView, PostArchiveView, PostDetailView
+from pigeon.views import PostListView, PostArchiveView, PostDetailView, BlogFeed
 
 urlpatterns = patterns('pigeon.views',
 
 	url(r'^$', PostListView.as_view()),
+	url(r'^rss/$', BlogFeed()),
 	url(r'^(?P<date_slug>[0-9_-]+)/$', PostArchiveView.as_view()),
-	url(r'^(?P<slug>[A-Za-z0-9_-]+)/$', PostDetailView.as_view(model=Post,)),
+	url(r'^(?P<slug>[A-Za-z0-9_-]+)/$', PostDetailView.as_view(model=Post,), name='post_view'),
 
 )
