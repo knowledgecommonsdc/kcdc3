f8763acfbea4214c8489d8fb6b096acdb0bfbe25
David Ramos <dave@imaginaryterrain.com>
2013-02-15 23:12:49 -0500

    Add department labels to blog

--- kcdc3/pigeon/models.py
+++ kcdc3/pigeon/models.py
@@ -18,13 +18,14 @@ class Post(models.Model):
 	featured = models.BooleanField(default=False)
 	date = models.DateTimeField()
 
-	thumbnail = models.ImageField(upload_to='post_images', blank=True, null=True)
+	department_label = models.CharField(max_length=128, blank=True)
+	thumbnail = models.ImageField(upload_to='blog', blank=True, null=True)
 	teaser = models.TextField(blank=True)
 	main_text = models.TextField(blank=True)
 
 	author = models.ManyToManyField(Bio, blank=True, null=True, related_name='author')
 	tags = models.CharField(max_length=128, blank=True)
-	allow_comments = models.BooleanField(default=True)
+	allow_comments = models.BooleanField(default=False)
 
 	class Meta:
 		ordering = ['date']
