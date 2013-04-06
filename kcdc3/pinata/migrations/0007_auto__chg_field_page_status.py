37df8f6f513bac5152cd08fff95b619fa8d10cf8
David Ramos <dave@imaginaryterrain.com>
2013-04-05 23:49:25 -0400

    Create wide page template

---
+++ kcdc3/pinata/migrations/0007_auto__chg_field_page_status.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Page.status'
+        db.alter_column('pinata_page', 'status', self.gf('django.db.models.fields.CharField')(max_length=48))
+
+    def backwards(self, orm):
+
+        # Changing field 'Page.status'
+        db.alter_column('pinata_page', 'status', self.gf('django.db.models.fields.CharField')(max_length=9))
+
+    models = {
+        'pinata.notice': {
+            'Meta': {'ordering': "['sort_order', 'title']", 'object_name': 'Notice'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'live': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'main_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': '50', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'pinata.page': {
+            'Meta': {'ordering': "['path']", 'object_name': 'Page'},
+            'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['pinata.Page']", 'null': 'True', 'blank': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
+            'short_title': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'sidebar_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': '50', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'basic.html'", 'max_length': '48'}),
+            'teaser': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'pinata.pressclipping': {
+            'Meta': {'ordering': "['date']", 'object_name': 'PressClipping'},
+            'date': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
+            'destination_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'excerpt': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'main_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'publication': ('django.db.models.fields.CharField', [], {'max_length': '200', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'PUBLISHED'", 'max_length': '9'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'pinata.slide': {
+            'Meta': {'ordering': "['sort_order', 'title']", 'object_name': 'Slide'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'live': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'main_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': '50', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'pinata.sponsor': {
+            'Meta': {'ordering': "['group', 'sort_order', 'title']", 'object_name': 'Sponsor'},
+            'destination_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'group': ('django.db.models.fields.CharField', [], {'default': "'B'", 'max_length': '3'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'main_text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': '50', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'PUBLISHED'", 'max_length': '9'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['pinata']
