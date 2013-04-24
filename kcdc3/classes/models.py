bb7e14d2b61375a72c5bd1c7ee55c3a4a5822fb6
David Ramos <dave@imaginaryterrain.com>
2013-04-24 16:49:00 -0400

    Add more info for image labels

--- kcdc3/classes/models.py
+++ kcdc3/classes/models.py
@@ -133,8 +133,8 @@ class Event(models.Model):
 	summary = models.TextField(blank=True)
 	description = models.TextField(blank=True)
 	details = models.TextField('Pre-class details', blank=True)
-	thumbnail = models.ImageField(upload_to='event_images', blank=True, null=True)
-	main_image = models.ImageField(upload_to='event_images', blank=True, null=True)
+	thumbnail = models.ImageField('Thumbnail (60x60px)', upload_to='event_images', blank=True, null=True)
+	main_image = models.ImageField('Main image (max 660px wide)', upload_to='event_images', blank=True, null=True)
 
 	email_welcome_text = models.TextField('Extra text for welcome email', blank=True)
 	email_reminder = models.BooleanField('Send reminder email?', default=True)
