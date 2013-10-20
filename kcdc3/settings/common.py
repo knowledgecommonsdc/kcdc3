6bd14d38ba2535eae7d88bca3211606822bbcf7e
www-data <www-data@knowledgecommonsdc.(none)>
2013-10-20 19:07:30 +0000

    Returning /static/ to service
    The /static/ directory needs to be sent by nginx,
    as it is static content and therefore gains nothing
    by being read through an intrepreted by python/django.

--- kcdc3/settings/common.py
+++ kcdc3/settings/common.py
@@ -36,11 +36,13 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = PROJECT_ROOT + '/../public/media/uploads/'
+# MEDIA_ROOT = PROJECT_ROOT + '/../public/media/uploads/'
+MEDIA_ROOT = '/srv/www/production/kcdc/kcdc3/kcdc3/public/media/uploads/'
 
 # Absolute filesystem path to the directory that will hold site design files
 # dave@imaginaryterrain.com
-ASSETS_ROOT = PROJECT_ROOT + '/../public/assets/'
+# ASSETS_ROOT = PROJECT_ROOT + '/../public/assets/'
+ASSETS_ROOT = '/srv/www/production/kcdc/kcdc3/kcdc3/public/assets'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -51,18 +53,21 @@ MEDIA_URL = 'http://localhost:8000/media/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT =  PROJECT_ROOT + "/../static"
+# STATIC_ROOT =  PROJECT_ROOT + "/../static"
+STATIC_ROOT = '/srv/www/production/kcdc/kcdc3/kcdc3/public/media/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+#STATIC_URL = None
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	PROJECT_ROOT + "/../public/static",
+	# PROJECT_ROOT + "/public/static",
+    "/srv/www/production/kcdc/kcdc3/kcdc3/public/static",
 )
 
 # List of finder classes that know how to find static files in
