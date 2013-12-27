02de0f5c724cb5f759feff97c6a2f96d96526fc6
David Ramos <dave@imaginaryterrain.com>
2013-12-26 22:09:40 -0500

    When running text through safestring filter, mark it as safe
    OK because all of the text comes from our own editorial team and is
    *supposed* to be marked as safe. We would need to handle this in a more
    sophisticated way if we were passing user-supplied text through this
    filter.

--- kcdc3/apps/pinata/templatetags/pinata_filters.py
+++ kcdc3/apps/pinata/templatetags/pinata_filters.py
@@ -1,5 +1,6 @@
 from django import template
 from smartypants import smartypants
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -8,9 +9,13 @@ def smartquotes(text):
     return smartypants(text)
 
 # Clean up text to match house style
-# This might need to be marked as safe
+#
+# We are marking the results as safe because the text is always 
+# going to be provided by the editorial team.
+# TODO: This would be dangerous if we ever had user-supplied text.
+# We'd need to be more careful to escape HTML.
 @register.filter
 def smartlines(text):
 	text = smartypants(text)
 	text = text.replace("<br /><br />","</p><p>")
-	return text
+	return mark_safe(text)
