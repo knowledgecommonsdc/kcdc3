51c42278115fbfbfbda55e84716e29c74fb46ee1
David Ramos <dave@imaginaryterrain.com>
2013-12-26 21:01:10 -0500

    Event description text: remove linebreaks, add smart quotes
    For event descriptions, add a filter that replaces double linebreaks
    with paragraphs, and also start using the smartypants filter

--- kcdc3/apps/pinata/templatetags/pinata_filters.py
+++ kcdc3/apps/pinata/templatetags/pinata_filters.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter
 def smartquotes(text):
     return smartypants(text)
+
+# This might need to be marked as safe
+@register.filter
+def remove_double_linebreaks(text):
+    return text.replace("<br /><br />","</p><p>")
+
