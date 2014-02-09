dce00c23411aa7cf753c0c2d4e47749ea5df4571
David Ramos <dave@imaginaryterrain.com>
2014-02-09 00:43:57 -0500

    Fix static files on localhost

--- kcdc3/settings/dev.py
+++ kcdc3/settings/dev.py
@@ -1,5 +1,11 @@
 from common import *
 
+# set up relative paths
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -19,3 +25,45 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = PROJECT_ROOT + '/../public/media/uploads/'
+
+# Absolute filesystem path to the directory that will hold site design files
+# dave@imaginaryterrain.com
+ASSETS_ROOT = PROJECT_ROOT + '/../public/assets/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = 'http://localhost:8000/media/uploads/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT =  PROJECT_ROOT + "/../static"
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+# STATIC_URL = 'http://localhost:8000/static/'
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+	# PROJECT_ROOT + "/public/static",
+	PROJECT_ROOT + "/../public/static",
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+   'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
