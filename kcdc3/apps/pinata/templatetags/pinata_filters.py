08e936a26e10e3873e17def88bff5a603bcc39a1
David Ramos <dave@imaginaryterrain.com>
2014-05-02 17:36:03 -0400

    Retrieve boilerplate text using template tag
    New template tag will insert some pieces of commonly-reused text

--- kcdc3/apps/pinata/templatetags/pinata_filters.py
+++ kcdc3/apps/pinata/templatetags/pinata_filters.py
@@ -1,8 +1,10 @@
 from django import template
 from smartypants import smartypants
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 register = template.Library()
+
 
 @register.filter
 def smartquotes(text):
@@ -19,3 +21,19 @@ def smartlines(text):
 	text = smartypants(text)
 	text = text.replace("<br /><br />","</p><p>")
 	return mark_safe(text)
+
+
+# Retrieve short phrases that are reused throughout the project
+# Right now, these phrases are stored in settings.py
+# Takes, as an argument, a string that identifies the phrase we want
+def boilerplate(slug):
+	
+	if slug == 'project_name':
+		return settings.PROJECT_NAME
+	elif slug == 'project_short_name':
+		return settings.PROJECT_SHORT_NAME
+	else:
+		return ''
+	
+register.simple_tag(boilerplate)
+
