e6e9339e448f22e59c83b416ee1c5cfd5ae24957
David Ramos <dave@imaginaryterrain.com>
2014-05-27 23:03:22 -0400

    Clean up display of locations in admin

--- kcdc3/apps/classes/admin.py
+++ kcdc3/apps/classes/admin.py
@@ -28,9 +28,9 @@ class RegistrationInline(admin.TabularInline):
 
 class LocationAdmin(admin.ModelAdmin):
 	model = Location
-	list_display = ('name','neighborhood','access','address1','lat','lng','show_exact',)
+	list_display = ('name','neighborhood','access','address1','zip','show_exact',)
 	list_filter = ('neighborhood',)
-	list_editable = ('lat', 'lng', 'access')
+	list_editable = ('zip',)
 	search_fields = ('name',)
 
 admin.site.register(Location, LocationAdmin)
