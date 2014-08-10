e125127916cdfc68a69837c0891c51e2b593f0a9
David Ramos <dave@imaginaryterrain.com>
2014-08-10 15:27:33 -0400

    Navbar details

--- frontend/app/assets/scripts/main.js
+++ frontend/app/assets/scripts/main.js
@@ -4,10 +4,10 @@ console.log('\'Allo \'Allo!');
 
 
 /* nav menu */
-
-$('.mobile-nav').hide();
-
-$('#show-mobile-nav').click(function() {
-	$('.mobile-nav').toggle(200);
-});
-
+// 
+// $('.mobile-nav').hide();
+//
+// $('#show-mobile-nav').click(function() {
+// 	$('.mobile-nav').toggle(200);
+// });
+//
