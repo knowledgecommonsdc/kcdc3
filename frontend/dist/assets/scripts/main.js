ab20889a8f11b90180fce7b381856c0c225f204b
David Ramos <dave@imaginaryterrain.com>
2014-11-23 21:41:24 -0500

    cleanup

--- frontend/dist/assets/scripts/main.js
+++ frontend/dist/assets/scripts/main.js
@@ -1,1 +1,1 @@
-console.log("'Allo 'Allo!");
+console.log("'Allo 'Allo!");var force=!1;$(".mobile-toggle__bd").click(function(a){a.preventDefault(),$(".navbar").slideToggle(200),force?$(".navbar").addClass("navbar--forced"):$(".navbar").removeClass("navbar--forced"),force=!force});
