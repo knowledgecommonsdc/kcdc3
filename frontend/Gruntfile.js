ab0cd8c6d1a09170035bedd2576bf1115284ff55
David Ramos <dave@imaginaryterrain.com>
2015-02-02 16:21:54 -0500

    build

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -112,30 +112,6 @@ module.exports = function (grunt) {
             server: '.tmp'
         },
 
-        // Make sure code styles are up to par and there are no obvious mistakes
-        // jshint: {
-//             options: {
-//                 jshintrc: '.jshintrc',
-//                 reporter: require('jshint-stylish')
-//             },
-//             all: [
-//                 'Gruntfile.js',
-//                 '<%= yeoman.app %>/assets/scripts/{,*/}*.js',
-//                 '!<%= yeoman.app %>/assets/scripts/vendor/*',
-//                 'test/spec/{,*/}*.js'
-//             ]
-//         },
-
-
-        // Mocha testing framework configuration options
-        // mocha: {
-        //     all: {
-        //         options: {
-        //             run: true,
-        //             urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
-        //         }
-        //     }
-        // },
 
 
 
